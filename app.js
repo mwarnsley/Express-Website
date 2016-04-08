cb57a7c1f17c728d9a93c2083e333aefa4b56f93
Warnsley <Marcus.Warnsley@navient.com>
2016-04-07 22:16:47 -0400

    Setting up the variable dependencies along with setting up the start for the package.json file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-ECHO is on.
+//setting up all the variables and adding the variable dependencies
+var express = require("express"),
+	path = require("path"),
+	bodyParser = require("body-parser"),
+	nodeMailer = require("nodemailer"),
+	port = 3000,
+	app = express();
+
+//setting up the middleware of node
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//running the server on port 3000
+app.listen(port);
+
+console.log("Server is running on port: " + port);
